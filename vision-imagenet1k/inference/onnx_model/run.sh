43d21a243ae2c691ef4ea78031a19dffd7515ec2
Bala Desinghu <bdesinghu@holylogin05.rc.fas.harvard.edu>
2025-05-05 09:56:26 -0400

    correcting onnx data path for imagenet1k

--- vision-imagenet1k/inference/onnx_model/run.sh
+++
@@ -1,9 +0,0 @@
-export DATA_PATH="/n/netscratch/kempner_dev/Everyone/workshop/ml-opt-workshop/data/cifar10"
-export CONDA_ENV="/n/netscratch/kempner_dev/Everyone/workshop/ml-opt-workshop/shared_env/pytorch_image"
-
-module load cuda
-module load cudnn
-module load python
-conda activate $CONDA_ENV
-
-python infer_imagenet1k_onnx.py --model final_model.onnx --image ./YellowLabradorLooking_new.jpg --labels imagenet_classes.txt --thread 4 
